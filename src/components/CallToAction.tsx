41d23bdf26ee75a881ca4b640f5b4b9394dd3a7a
wwzeng1 <44910023+wwzeng1@users.noreply.github.com>
2023-11-15 23:31:35 -0800

    init

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -85,21 +85,36 @@ export default function CallToAction() {
           lineHeight={"110%"}
           mt="0 !important"
         >
-          <TypeAnimation
+          Sweep: <TypeAnimation
             sequence={[
-              "Unit test",
-              3000,
+              "Unit Test",
+              2000,
+              4000,
               'Refactor',
-              3000,
+              4000,
               'Debug',
-              3000,
+              4000,
             ]}
             wrapper="span"
             speed={50}
             repeat={Infinity}
             cursor={false}
           />
-          &nbsp;Python with AI
+          &nbsp;my&nbsp;
+          <TypeAnimation
+            sequence={[
+              "data pipeline",
+              4000,
+              'flask app',
+              4000,
+              'ML pipeline',
+              4000,
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+            cursor={false}
+          />
         </Heading>
         <Text
           color={"purple.400"}
@@ -107,7 +122,7 @@ export default function CallToAction() {
           mt="1rem !important"
           mb="1rem !important"
         >
-          Junior Python developer that uses your entire codebase to write Pull Requests
+          AI Junior Developer that cleans up your Python code
         </Text>
         <Button
           color="white"
