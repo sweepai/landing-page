4d54f9c8f52deef5cd2f04f8988b33a9d5805bed
Kevin Lu <kevinlu1248@gmail.com>
2024-03-11 18:15:44 -0700

    Final Sweep pricing page

--- src/components/PricingPage.tsx
+++ src/components/PricingPage.tsx
@@ -8,17 +8,20 @@ const PricingPage = () => (
     <Text fontSize="2xl" mb={16}>
       Get started with the leading software maintenance AI.
     </Text>
-    <SimpleGrid columns={{ base: 1, md: 3 }} px={16}>
+    <SimpleGrid columns={{ base: 1, md: 3 }} px={32} spacing={16}>
       <Box
         display="flex"
         flexDirection="column"
         alignItems="center"
         justifyContent="space-between"
+        height="100%"
       >
         <Box
+          height="100%"
           width="100%"
           borderRadius={10}
           backgroundColor="rgba(28, 27, 47, 0.8)"
+          // backgroundColor="rgba(74, 85, 104, 0.5)"
           p={12}
           mx={4}
           mb={12}
@@ -37,12 +40,6 @@ const PricingPage = () => (
             Install
           </Button>
         </Box>
-        <Box as="ul" style={{ listStyleType: 'none' }} color="gray.400" fontSize="lg">
-          <li>✔ 5 GPT-4 Sweep tickets / month</li>
-          <li>✔ Self-host Sweep for free</li>
-          <br />
-          <li><em>Purchase additional GPT-4 tickets <Link href="https://buy.stripe.com/00g3fh7qF85q0AE14d" color="#89CFF0">here</Link>.</em></li>
-        </Box>
       </Box>
 
       <Box
@@ -50,6 +47,7 @@ const PricingPage = () => (
       >
         <Box
           height="100%"
+          width="100%"
           borderRadius={10}
           backgroundColor="rgba(28, 27, 47, 0.8)"
           p={12}
@@ -70,11 +68,6 @@ const PricingPage = () => (
             Purchase
           </Button>
         </Box>
-        <Box as="ul" style={{ listStyleType: 'none' }} color="gray.400" fontSize="lg">
-          <li>✔ Unlimited GPT-4 Sweep issues</li>
-          <li>✔ Faster search with live re-indexing</li>
-          <li>✔ Commercial SLA</li>
-        </Box>
       </Box>
 
       <Box
@@ -82,6 +75,7 @@ const PricingPage = () => (
       >
         <Box
           height="100%"
+          width="100%"
           borderRadius={10}
           backgroundColor="rgba(28, 27, 47, 0.8)"
           p={12}
@@ -102,26 +96,19 @@ const PricingPage = () => (
             Contact us
           </Button>
         </Box>
-        <Box as="ul" style={{ listStyleType: 'none' }} color="gray.400" fontSize="lg">
-          <li>✔ Unlimited GPT-4 Sweep issues</li>
-          <li>✔ Fine-tuned retrieval model</li>
-          <li>✔ Self-hosted backend</li>
-          <li>✔ Enterprise support</li>
-        </Box>
       </Box>
     </SimpleGrid>
     <SimpleGrid columns={{ base: 1, md: 3 }} px={16}>
       <Box as="ul" style={{ listStyleType: 'none' }} color="gray.400" fontSize="lg">
-        <li>✔ Unlimited GPT-4 Sweep issues</li>
-        <li>✔ Fine-tuned retrieval model</li>
-        <li>✔ Self-hosted backend</li>
-        <li>✔ Enterprise support</li>
+        <li>✔ 5 GPT-4 Sweep tickets / month</li>
+        <li>✔ Self-host Sweep for free</li>
+        <br />
+        <li><em>Purchase additional GPT-4 tickets <Link href="https://buy.stripe.com/00g3fh7qF85q0AE14d" color="#89CFF0">here</Link>.</em></li>
       </Box>
       <Box as="ul" style={{ listStyleType: 'none' }} color="gray.400" fontSize="lg">
         <li>✔ Unlimited GPT-4 Sweep issues</li>
-        <li>✔ Fine-tuned retrieval model</li>
-        <li>✔ Self-hosted backend</li>
-        <li>✔ Enterprise support</li>
+        <li>✔ Faster search with live re-indexing</li>
+        <li>✔ Commercial SLA</li>
       </Box>
       <Box as="ul" style={{ listStyleType: 'none' }} color="gray.400" fontSize="lg">
         <li>✔ Unlimited GPT-4 Sweep issues</li>
